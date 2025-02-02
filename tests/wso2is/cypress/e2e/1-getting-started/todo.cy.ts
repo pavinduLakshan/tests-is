72adc1006492f2a2fcfbbef4865e85a195e2147b
Pavindu Lakshan <pavindulakshan@gmail.com>
2025-02-02 12:44:54 +0530

    Restructure folder structure and remove build scripts

--- tests/wso2is/cypress/e2e/1-getting-started/todo.cy.ts
+++ tests/wso2is/cypress/e2e/1-getting-started/todo.cy.ts
@@ -1,15 +1,7 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
+import { CypressTaskConfig } from "@wso2iam/test-cypress-core/configs/cypress-task-config"
+import { ProductConfig } from "@wso2iam/test-ts-core/configs/product-config"
 
 describe('example to-do app', () => {
   beforeEach(() => {
@@ -24,6 +16,7 @@ describe('example to-do app', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
+    cy.log(CypressTaskConfig.taskAgentStartup)
     cy.get('.todo-list li').should('have.length', 2)
 
     // We can go even further and check that the default todos each contain
